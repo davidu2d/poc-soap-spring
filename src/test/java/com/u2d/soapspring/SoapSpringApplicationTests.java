115b9d1d252d76bd2235b15766eb0aa01f385e07
davidu2d <u2dtecnologia@gmail.com>
2023-09-13 21:31:18 -0300

    first commit

---
+++ src/test/java/com/u2d/soapspring/SoapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u2d.soapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
